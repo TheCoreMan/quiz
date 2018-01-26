2d983b2ef3d23d0da4017b3161ef0ca68036080d
TheCoreMan <dude500@gmail.com>
2018-01-26 22:32:45 +0200

    initial commit works, no file flags, no refactoring at all

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type Problem struct {
+	Question string
+	Solution string
+}
+
+func main() {
+	fmt.Println("Welcode to the quiz!")
+	var problems []Problem
+	problems = getProblems(problems)
+
+	correctAnswers := 0
+	stdinReader := bufio.NewReader(os.Stdin)
+	for index, problem := range problems {
+		fmt.Printf("Problem #%d:\t%s\t=\t", index, problem.Question)
+		userSolution, err := stdinReader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+
+		userSolution = strings.TrimSpace(userSolution)
+		if userSolution == problem.Solution {
+			correctAnswers = correctAnswers + 1
+			fmt.Println("Good")
+		} else {
+			fmt.Println("Mistake")
+		}
+	}
+
+	fmt.Printf("\nFinal score: %d/%d.", correctAnswers, len(problems))
+}
+
+func getProblems(problems []Problem) []Problem {
+	problemsFilePath := "./problems.csv"
+	// TODO change with flag and use filepath to verify stuff
+	problemsFile, err := os.Open(problemsFilePath)
+	if err != nil {
+		panic(err)
+	}
+	csvReader := csv.NewReader(bufio.NewReader(problemsFile))
+	for {
+		line, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+
+		problems = append(problems, Problem{Question: line[0], Solution: line[1]})
+	}
+	return problems
+}
